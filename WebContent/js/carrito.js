ff0576d22bfbe97177b7dfee9a82a15ecc666236
br99bry <bryanblack1999@gmail.com>
2021-09-09 12:15:52 -0500

    logica funcional implementada para calcular total a pagar por el cliente

--- WebContent/js/carrito.js
+++ WebContent/js/carrito.js
@@ -1,20 +1,47 @@
-const agregarButton = document.querySelectorAll('.carrito__add-quantity');
-const eliminarButton = document.querySelectorAll('.carrito__substract-quantity');
+const productsCart = document.querySelectorAll('.carrito-product');
 
-agregarButton.forEach(button => {
+const calSubTotal = () => {
+  const subTotalBox = document.getElementById('subtotal');
+  const items = document.querySelectorAll('.carrito__items-total p i');
+  let subTotal = 0;
+  items.forEach( item => {
+    subTotal+= parseFloat(item.innerText);
+  } )
+  subTotalBox.innerText=`${subTotal}`
+}
+
+const calcItems = (product) => {
+  const quantityManage = product.querySelector('.quantity');
+  let quantity = parseInt(quantityManage.innerText);
+  const unitPriceBox = product.querySelector('.carrito__items-precio p i');
+  const unitPrice = parseFloat(unitPriceBox.innerText);
+  const quantityPriceBox = product.querySelector('.carrito__items-total p i');
+  let quantityPrice = unitPrice * quantity;
+  quantityPriceBox.innerText=`${quantityPrice}`
+  calSubTotal();
+}
+
+
+productsCart.forEach(product => {
+  calcItems(product);
+  const button = product.querySelector('.carrito__add-quantity');
   button.addEventListener( 'click', () =>{
-    let quantity = button.parentNode.querySelector('.quantity').textContent;
-    quantity= parseInt(quantity) + 1;
+    let quantity = button.parentNode.querySelector('.quantity').innerText;
+    quantity = parseInt(quantity) + 1;
     const number = button.parentNode.querySelector('.quantity');
     number.innerText=`${quantity}`;
+    calcItems(product);
   } )
 });
 
-eliminarButton.forEach(button => {
+productsCart.forEach(product => {
+  const button = product.querySelector('.carrito__substract-quantity');
+  calcItems(product);
   button.addEventListener( 'click', () =>{
-    let quantity = button.parentNode.querySelector('.quantity').textContent;
+    let quantity = button.parentNode.querySelector('.quantity').innerText;
     quantity = parseInt(quantity) - 1;
     const number = button.parentNode.querySelector('.quantity');
     number.innerText=`${quantity}`;
+    calcItems(product);
   } )
 });
